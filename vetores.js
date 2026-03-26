b8dc0795b63086611a5f4cd8c003121542c89ea6
ariel martins <ariel.m.conceicao@ba.estudante.senai.br>
2026-03-26 20:35:33 -0300

    excluindo arquivo teste

--- vetores.js
+++
@@ -1,8 +0,0 @@
-let cidades = ["Madrid = 16", "Milano = 10", "Liverpool = 6", "Munique = 6", "Barcelona = 5"]
-console.log("Cidades com mais Champions League")
-console.log(cidades[0])
-console.log(cidades[1])
-console.log(cidades[2])
-console.log(cidades[3])
-console.log(cidades[4])
-console.log()
